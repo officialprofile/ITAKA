5527f3050a0b1627d1eed5588414b22167b0795b
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-11-06 12:57:13 +0100

    Create analyze_all_profiles.py

---
+++ analyze_all_profiles.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import sync_playwright
+
+def analyze_profile(profile_url):
+  profile_data = []
+  with sync_playwright() as p:
+    browser = p.chromium.launch(headless = True)
+    page = browser.new_page()
+    page.goto(profile_url)
+
+    name = page.query_selector('//div[contains(@class, "content")]/h1')
+    profile_data.append(name.text_content())
+    attributes = page.query_selector_all('//div[contains(@class, "right")]')
+    [profile_data.append(attribute.text_content()) for attribute in attributes]
+    browser.close()
+  return(';'.join(profile_data)+'\n')
+
+
+def analyze_all_profiles(text_file_name):
+  raw_data = open('raw_data.txt', 'a')
+  
+  with open(text_file_name, 'r') as links:
+    for i, link in enumerate(links):
+      raw_data.write(analyze_profile(link))
+      print(i+1)
+    links.close()
+  raw_data.close()
+
+analyze_all_profiles('links.txt')

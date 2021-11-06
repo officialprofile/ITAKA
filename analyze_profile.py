9e2305b72d25bb7d93eef9f2998bcab485771993
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-11-06 10:11:29 +0100

    Update analyze_profile.py

--- analyze_profile.py
+++ analyze_profile.py
@@ -1,25 +1,30 @@
 from playwright.sync_api import sync_playwright
 
-# This function returns 8 elements in the following order: 
-#     1. aktualny wiek
-#     2. data zaginiecia
-#     3. wiek w dniu zaginiecia, 
-#     4. wzrost
-#     5. kolor oczu
-#     6. znaki szczegolne
-#     7. ostatnie miejsce pobytu
-#     8. kraj
+# The function returns 9 elements:
+#     1. Zaginiony(a): imie drugie imie nazwisko
+#     2. aktualny wiek
+#     3. data zaginiecia
+#     4. wiek w dniu zaginiecia, 
+#     5. wzrost
+#     6. kolor oczu
+#     7. znaki szczegolne
+#     8. ostatnie miejsce pobytu
+#     9. kraj
 
-def analyze_profile(url):
+def analyze_profile(profile_url):
+  labels = ['Imie', 'Aktualny wiek', 'Data zaginiecia', 'Wiek w dniu zaginiecia', 'Wzrost', 'Kolor oczu', 'Znaki szczegolne', 'Ostatnie miejsce pobytu', 'Kraj']
   profile_data = []
   with sync_playwright() as p:
     browser = p.chromium.launch(headless = True)
     page = browser.new_page()
-    page.goto(url)
+    page.goto(profile_url)
+
+    name = page.query_selector('//div[contains(@class, "content")]/h1')
+    profile_data.append(name.text_content())
     attributes = page.query_selector_all('//div[contains(@class, "right")]')
     [profile_data.append(attribute.text_content()) for attribute in attributes]
     browser.close()
-  return(profile_data)
+  return(dict(zip(labels, profile_data)))
 
-example_url = 'http://www.zaginieni.pl/jak-pomagamy/poszukiwanie-zaginionych/zagineli/profil-osoby/?o=22386'
-print(analyze_profile(example_url))
+link = 'http://www.zaginieni.pl/jak-pomagamy/poszukiwanie-zaginionych/zagineli/profil-osoby/?o=22386'
+print(analyze_profile(link))

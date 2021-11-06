46e111d90af1b25c5dbfe835473c003d89e4cdf8
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-11-06 09:51:41 +0100

    After tens of successful loops "newtorkidle" appears

--- get_urls.py
+++ get_urls.py
@@ -1,31 +1,31 @@
 from playwright.sync_api import sync_playwright
 
-# The function returns 8 elements: 
-#     1. aktualny wiek
-#     2. data zaginiecia
-#     3. wiek w dniu zaginiecia, 
-#     4. wzrost
-#     5. kolor oczu
-#     6. znaki szczegolne
-#     7. ostatnie miejsce pobytu
-#     8. kraj
-
-def analyze_profile(profile_url):
-  profile_data = []
+def get_urls():
+  text_file = open('links.txt', 'a')
   with sync_playwright() as p:
     browser = p.chromium.launch(headless = True)
-    page = browser.new_page()
-    page.goto(profile_url)
-    attributes = page.query_selector_all('//div[contains(@class, "right")]')
-    [profile_data.append(attribute.text_content()) for attribute in attributes]
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("http://www.zaginieni.pl/jak-pomagamy/poszukiwanie-zaginionych/zagineli/wyniki-wyszukiwania/?firstname=&lastname=&type=missing&height_from=&height_to=&eyes=&age_from=&age_to=&age=now&city=&district=")
+
+    current_page = 1
+    last_page = page.query_selector('//p[@class="search_result_pagination"]/a[last()]').text_content()
+
+    profiles = page.query_selector_all('//a[contains(@href, "profil-osoby")]')
+
+    for profile in profiles:
+      text_file.write(profile.get_attribute('href')+'\n')
+
+    while current_page < int(last_page): #int(last_page)
+      print(current_page)
+      page.click('//p[@class="search_result_pagination"]/a[contains(@href,'+ str(current_page + 1) + ')]')
+      page.wait_for_load_state()
+      profiles = page.query_selector_all('//a[contains(@href, "profil-osoby")]')
+      for profile in profiles:
+        text_file.write(profile.get_attribute('href')+'\n')
+      current_page += 1
+
     browser.close()
-  return(profile_data)
+  text_file.close()
 
-# example_link = 'http://www.zaginieni.pl/jak-pomagamy/poszukiwanie-zaginionych/zagineli/profil-osoby/?o=22386'
-# print(analyze_profile(example_link))
-
-with open("links.txt", 'r') as links:
-    for i, link in enumerate(links):
-      print(analyze_profile(link.rstrip()))
-      if i > 3:
-        break
+get_urls()

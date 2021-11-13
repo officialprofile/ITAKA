a110a677c46a0811cbcf70ab24279caced03b4de
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-11-13 10:37:20 +0100

    Handling missing urls

--- analyze_all_profiles.py
+++ analyze_all_profiles.py
@@ -6,14 +6,14 @@ def analyze_profile(profile_url):
     browser = p.chromium.launch(headless = True)
     page = browser.new_page()
     page.goto(profile_url)
-
     name = page.query_selector('//div[contains(@class, "content")]/h1')
+    if (name == None):
+      return('')
     profile_data.append(name.text_content())
     attributes = page.query_selector_all('//div[contains(@class, "right")]')
     [profile_data.append(attribute.text_content()) for attribute in attributes]
     browser.close()
   return(';'.join(profile_data)+'\n')
-
 
 def analyze_all_profiles(text_file_name):
   raw_data = open('raw_data.txt', 'a')

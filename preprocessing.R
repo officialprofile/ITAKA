794eb7d6d4aa3b671d8fb7dc36d6993c140732fb
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-11-16 08:20:08 +0100

    Add files via upload

--- preprocessing.R
+++ preprocessing.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(lubridate)
 
 data = read.csv('raw_data.txt', sep = ';', header = F)
 colnames(data) = c('Imie i nazwisko', 'Wiek obecny', 'Data zaginiecia', 'Wiek', 'Wzrost', 
@@ -7,9 +8,19 @@ colnames(data) = c('Imie i nazwisko', 'Wiek obecny', 'Data zaginiecia', 'Wiek', 
 data$`Wiek obecny`      <- sapply(data$`Wiek obecny`, function(x) {as.numeric(strsplit(x, ' ')[[1]][2])})
 data$Wiek               <- sapply(data$Wiek, function(x) {as.numeric(strsplit(x, ' ')[[1]][2])})
 data$Wzrost             <- sapply(data$Wzrost, function(x) {as.numeric(strsplit(x, ' ')[[1]][1])})
-data$`Data zaginiecia`  <- sapply(data$`Data zaginiecia`, function(x) {as.Date(x, fromat = "%Y/%m/%d")})
+
 data$`Plec`             <- sapply(data$`Imie i nazwisko`, function(x) {ifelse(strsplit(x, ' ')[[1]][1] == 'Zaginiony:', 'M', 'K')})
 data$`Nazwisko`         <- sapply(data$`Imie i nazwisko`, function(x) {y = strsplit(x, ' ')[[1]]; y[length(y)]})
 data$`Imie`             <- sapply(data$`Imie i nazwisko`, function(x) {strsplit(x, ' ')[[1]][2]})
 
-write.csv(data, 'data.csv')
+data$Rok                <- sapply(data$`Data zaginiecia`, function(x) {year(x)})
+data$Miesiac            <- sapply(data$`Data zaginiecia`, function(x) {month(x)})
+data$Dzien              <- sapply(data$`Data zaginiecia`, function(x) {day(x)})
+data$`Dzien tygodnia`   <- sapply(data$`Data zaginiecia`, function(x) {wday(x)})
+
+
+data <- data[, c('Imie', 'Nazwisko', 'Plec', 'Wiek', 'Wiek obecny', 'Wzrost', 'Kolor oczu', 
+                 'Ostatnie miejsce pobytu', 'Kraj', 'Data zaginiecia', 'Rok', 'Miesiac', 
+                 'Dzien', 'Dzien tygodnia', 'Znaki szczegolne')]
+
+write.csv(data, 'data.csv', row.names = F)
